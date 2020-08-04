44c1b357af7beeb94e3291c8ab12b18f4ee34d4a
Karlos97 <k.minta97@gmail.com>
2020-08-04 16:36:38 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+
+// import Search from './models/Search';
+// import * as searchView from './views/searchView';
+// import { elements, renderLoader, clearLoader } from './views/base';
+//import '../sass/main.scss';
+import '../css/style.scss';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
